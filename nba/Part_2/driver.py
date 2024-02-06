a02c9692b5f57f98a2276833c5c1163724f238ee
Ramxnan <ramanan.palur@gmail.com>
2024-02-06 20:49:53 +0530

    calculated indirect

--- nba/Part_2/driver.py
+++ nba/Part_2/driver.py
@@ -13,6 +13,7 @@ from .printout import printout
 import os
 import uuid
 from openpyxl import load_workbook
+import pandas as pd
 
 def driver_part2(input_dir_path, output_dir_path):
     #create openpyxl workbook
@@ -20,18 +21,22 @@ def driver_part2(input_dir_path, output_dir_path):
     wbwrite.remove(wbwrite.active)
     excel_files=[]
     total_students = 0
+
+    sum_indirect_assessment = pd.DataFrame()
+
     for file in os.listdir(input_dir_path):
-        if file.endswith(".xlsx") and not file.startswith("Combined"):
+        if file.endswith(".xlsx") and not file.startswith("Sum"):
             excel_files.append(file)
     excel_files.sort(reverse=True)
 
     for file in excel_files:
-        if file.endswith(".xlsx") and not file.startswith("Combined"):
+        if file.endswith(".xlsx") and not file.startswith("Sum"):
             file_path = os.path.join(input_dir_path, file)
             wbread = load_workbook(file_path, data_only=True)
 
             alldata={}
             Component_Details = {}
+            indirect_assessment = pd.DataFrame()
             for sheet_name in wbread.sheetnames:
                 if sheet_name.endswith("Input_Details"):
                     input_details_title = sheet_name
@@ -46,7 +51,7 @@ def driver_part2(input_dir_path, output_dir_path):
 
             total_students+=alldata['Number_of_Students']
             data = {key: alldata[key] for key in alldata.keys() & {'Teacher', 'Academic_year', 'Batch', 'Branch', 'Subject_Name', 'Subject_Code', 'Section', 'Semester', 'Number_of_Students', 'Number_of_COs'}}
-            combined_data = alldata
+            sum_data_all = alldata
 
 
 
@@ -55,10 +60,21 @@ def driver_part2(input_dir_path, output_dir_path):
             for row in wsread_input_details[table_range][1:]:
                 Component_Details[row[0].value] = row[1].value
 
-            combined_Component_Details = Component_Details
+            sum_Component_Details = Component_Details
 
-            
+            #get values from wsread_input_details from cell E{2+number_of_COs+4+1} to E{2+number_of_COs+4+1+number_of_COs} and concat it to sum_indirect_assessment
+            start_row = 2 + data["Number_of_COs"] + 4 + 1
+            end_row = 2 + data["Number_of_COs"] + 4 + data["Number_of_COs"]
+            cell_range = f'E{start_row}:E{end_row}'
 
+            values = []
+            for row in wsread_input_details[cell_range]:
+                for cell in row:
+                    values.append(cell.value)
+
+            indirect_assessment=pd.DataFrame(values, columns=[f"{data['Section']}"])
+            sum_indirect_assessment = pd.concat([sum_indirect_assessment, indirect_assessment], axis=1)
+                                                         
             wbwrite.create_sheet(f"{data['Section']}_Input_Details")
             wswrite = wbwrite[f"{data['Section']}_Input_Details"]
             wswrite = input_detail(data,Component_Details,wswrite)
@@ -115,58 +131,70 @@ def driver_part2(input_dir_path, output_dir_path):
 
             wbread.close()
     #save the workbook
+    
+    sum_data_all['Section'] = "Sum"
+    sum_data_all['Number_of_Students'] = total_students
+    sum_data = {key: sum_data_all[key] for key in sum_data_all.keys() & {'Teacher', 'Academic_year', 'Batch', 'Branch', 'Subject_Name', 'Subject_Code', 'Section', 'Semester', 'Number_of_Students', 'Number_of_COs'}}
+
+    sum_Component_Details = {f"{sum_data['Section']}_{key[2:]}":value for key,value in Component_Details.items()}
+
     print(total_students)
-    print(combined_data)
-    # combined_data['Section'] = "Combined"
-    # combined_data['Number_of_Students'] = total_students
+    print(sum_data_all)
+    print(sum_Component_Details)
+    print(sum_indirect_assessment)
 
+    wbwrite.create_sheet(f"{sum_data['Section']}_Input_Details")
+    wswrite = wbwrite[f"{sum_data['Section']}_Input_Details"]
+    wswrite['B14'] = sum_data_all['Default Threshold %']
+    wswrite['B15'] = sum_data_all['Internal %']
+    wswrite['B17'] = sum_data_all['Direct %']
+    wswrite['B19'] = sum_data_all['Target CO Attainment %']
 
-    # wbwrite.create_sheet("Combined_Input_Details")
-    # wswrite = wbwrite["Combined_Input_Details"]
-    # wswrite['B14'] = combined_data['Default Threshold %']
-    # wswrite['B15'] = combined_data['Internal %']
-    # wswrite['B17'] = combined_data['Direct %']
-    # wswrite['B19'] = combined_data['Target CO Attainment %']
-    # wswrite = input_detail(combined_data,Component_Details,wswrite)
+    #add a column which is average of all the columns in sum_indirect_assessment
+    sum_indirect_assessment['Avg'] = sum_indirect_assessment.mean(axis=1)
+    for i in range(len(sum_indirect_assessment)):
+        wswrite[f'E{2+sum_data['Number_of_COs']+4+1+i}'] = sum_indirect_assessment['Avg'][i]
+
+    wswrite = input_detail(sum_data,sum_Component_Details,wswrite)
     
 
-    # wswrite = indirect_co_assessment(combined_data,wswrite)
-    # adjust_width(wswrite)
-    # wswrite = CO_PO_Table(combined_data,wswrite)
+    wswrite = indirect_co_assessment(sum_data,wswrite)
+    adjust_width(wswrite)
+    wswrite = CO_PO_Table(sum_data,wswrite)
 
-    # for key in combined_Component_Details.keys():
-    #     #replace first letter with combined
-    #     sheet_name="Combined"+key[1:] 
-    #     wbwrite.create_sheet(sheet_name)
-    #     wswrite = wbwrite[sheet_name]
-    #     wswrite.title = sheet_name
-    #     wswrite = qn_co_mm_btl(combined_data, key, combined_Component_Details[key], wswrite)
-    #     wswrite = studentmarks(combined_data, key, combined_Component_Details[key], wswrite)
 
-    #     wswrite = cummulative_co_mm_btl(combined_data, key, combined_Component_Details[key], wswrite)
-    #     wswrite = cummulative_studentmarks(combined_data, key, combined_Component_Details[key], wswrite)
+    for key in sum_Component_Details.keys():
+        #replace first letter with combined
+        wbwrite.create_sheet(key)
+        wswrite = wbwrite[key]
+        wswrite.title = key
+        wswrite = qn_co_mm_btl(sum_data, key, sum_Component_Details[key], wswrite)
+        wswrite = studentmarks(sum_data, key, sum_Component_Details[key], wswrite)
 
-    # wbwrite.create_sheet("Combined_Internal_Components")
-    # wswrite = wbwrite["Combined_Internal_Components"]
-    # wswrite = Component_calculation(combined_data,combined_Component_Details,wswrite,"I")
+        wswrite = cummulative_co_mm_btl(sum_data, key, sum_Component_Details[key], wswrite)
+        wswrite = cummulative_studentmarks(sum_data, key, sum_Component_Details[key], wswrite)
 
-    # wbwrite.create_sheet("Combined_External_Components")
-    # wswrite = wbwrite["Combined_External_Components"]
-    # wswrite = Component_calculation(combined_data,combined_Component_Details,wswrite,"E")
+    wbwrite.create_sheet("Sum_Internal_Components")
+    wswrite = wbwrite["Sum_Internal_Components"]
+    wswrite = Component_calculation(sum_data,sum_Component_Details,wswrite,"I")
 
-    # wbwrite.create_sheet("Combined_Course_level_Attainment")
-    # wswrite = wbwrite["Combined_Course_level_Attainment"]
-    # wswrite=write_course_level_attainment(combined_data, combined_Component_Details, wswrite)
+    wbwrite.create_sheet("Sum_External_Components")
+    wswrite = wbwrite["Sum_External_Components"]
+    wswrite = Component_calculation(sum_data,sum_Component_Details,wswrite,"E")
 
-    # wbwrite.create_sheet("Combined_Printout")
-    # wswrite = wbwrite["Combined_Printout"]
-    # wswrite=printout(wswrite,combined_data)
+    wbwrite.create_sheet("Sum_Course_level_Attainment")
+    wswrite = wbwrite["Sum_Course_level_Attainment"]
+    wswrite=write_course_level_attainment(sum_data, sum_Component_Details, wswrite)
+
+    wbwrite.create_sheet("Sum_Printout")
+    wswrite = wbwrite["Sum_Printout"]
+    wswrite=printout(wswrite,sum_data)
 
 
 
 
     unique_id = str(uuid.uuid4())
-    excel_file_name = f"Combined_{unique_id}.xlsx"
+    excel_file_name = f"Sum_{unique_id}.xlsx"
     wbwrite.save(os.path.join(output_dir_path, excel_file_name))
 
 if __name__ == "__main__":
